bb3fa0bc4ac05bc94eab6342c3e8d01b6b44f7b8
Lars Kellogg-Stedman <lars@oddbit.com>
2022-03-04 13:33:07 -0500

    Generate login password when adding user

--- guac/cmd/add_user.py
+++ guac/cmd/add_user.py
@@ -5,17 +5,37 @@ import subprocess
 import re
 
 import guac.models
+import guac.utils
 
 LOG = logging.getLogger(__name__)
 
 
 @click.option("--keydir", "-k", default="keys")
+@click.option("--recreate-password", "--rp", is_flag=True)
 @click.option("--recreate-key", "--rk", is_flag=True)
 @click.option("--recreate-user", "--ru", is_flag=True)
 @click.option("--recreate-connection", "--rc", is_flag=True)
 @click.argument("username")
 @click.pass_obj
-def add_user(api, keydir, recreate_key, recreate_user, recreate_connection, username):
+def add_user(
+    api,
+    keydir,
+    recreate_password,
+    recreate_key,
+    recreate_user,
+    recreate_connection,
+    username,
+):
+    password_file = os.path.join(keydir, f"{username}.password")
+    if not os.path.exists(password_file) or recreate_password:
+        LOG.warning("creating password for user %s", username)
+        with open(password_file, "w") as fd:
+            fd.write(guac.utils.random_string(25))
+            fd.write("\n")
+
+    with open(password_file) as fd:
+        password = fd.read().strip()
+
     if not os.path.exists(os.path.join(keydir, username)) or recreate_key:
         LOG.warning("creating key for user %s", username)
         try:
@@ -78,6 +98,7 @@ def add_user(api, keydir, recreate_key, recreate_user, recreate_connection, user
             parameters=guac.models.RDPConnectionParameters(
                 hostname=f"{slug}-vm-int",
                 username="fedora",
+                password=password,
             ),
         )
         api.connection_add(c.dict(by_alias=True))
