e7c6ca719e555b473f55bfa34abb395db47a6262
Lars Kellogg-Stedman <lars@oddbit.com>
2022-03-03 23:32:14 -0500

    Add delete-user command

--- guac/cmd/main.py
+++ guac/cmd/main.py
@@ -105,5 +105,22 @@ def add_user(api, keydir, recreate_key, recreate_user, recreate_connection, user
     api.user_grant_connection(username, rdp_connection_name)
 
 
+@main.command()
+@click.argument("username")
+@click.pass_obj
+def delete_user(api, username):
+    slug = re.sub("[^a-z0-9-]+", "-", username)
+    ssh_connection_name = f"{slug}-vm-ssh"
+    rdp_connection_name = f"{slug}-vm-rdp"
+
+    LOG.warning("delete user %s", username)
+    api.user_delete(username)
+
+    for conname in [ssh_connection_name, rdp_connection_name]:
+        LOG.warning("delete connection %s", conname)
+        api.connection_delete(conname)
+    api.connection_delete(rdp_connection_name)
+
+
 if __name__ == "__main__":
     main()
